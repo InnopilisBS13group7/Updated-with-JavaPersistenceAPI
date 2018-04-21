443b766e221e323f44694819292fb4ae707ae171
Semen Kiselev <s.kiselev@innopolis.ru>
2018-04-21 20:32:51 +0300

    back for search

--- SpringLybrary/src/main/java/net/proselyte/springsecurityapp/model/Document.java
+++ SpringLybrary/src/main/java/net/proselyte/springsecurityapp/model/Document.java
@@ -4,6 +4,8 @@ import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 
 @Entity
 @Table(name = "documents")
@@ -157,5 +159,43 @@ public class Document {
                 ", status='" + status + '\'' +
                 ", amount=" + amount +
                 "}\n";
+    }
+
+    public boolean isAppropriateForSearch(String s){
+        boolean check = false;
+        String doc = id + ' ' + title + ' ' + author + ' ' + teg + ' ' + publisher;
+        String[] l = doc.split(" ");
+        List<Document> list = new LinkedList<>();
+        for (String i : l){
+            if (editdist(i,s) < 3) check = true;
+        }
+        return check;
+    }
+
+    public static int editdist(String S1, String S2) {
+        int m = S1.length(), n = S2.length();
+        int[] D1;
+        int[] D2 = new int[n + 1];
+
+        for(int i = 0; i <= n; i ++)
+            D2[i] = i;
+
+        for(int i = 1; i <= m; i ++) {
+            D1 = D2;
+            D2 = new int[n + 1];
+            for(int j = 0; j <= n; j ++) {
+                if(j == 0) D2[j] = i;
+                else {
+                    int cost = (S1.charAt(i - 1) != S2.charAt(j - 1)) ? 1 : 0;
+                    if(D2[j - 1] < D1[j] && D2[j - 1] < D1[j - 1] + cost)
+                        D2[j] = D2[j - 1] + 1;
+                    else if(D1[j] < D1[j - 1] + cost)
+                        D2[j] = D1[j] + 1;
+                    else
+                        D2[j] = D1[j - 1] + cost;
+                }
+            }
+        }
+        return D2[n];
     }
 }
